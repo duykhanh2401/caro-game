1b00af419314efc5df4e66c67fed2a886fbe54c1
duykhanh2401 <vuduykhanh3@gmail.com>
2024-01-13 00:02:36 +0700

    update home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-	<DKModal
-		:isOpen="true"
-		:centered="true"
-		:disableClose="true"
-		title="Chào mừng bạn đến với game cờ Caro"
-	>
-		<DKTextInput
-			label="Vui lòng nhập tên của bạn:"
-			placeholder="Tên của bạn..."
-		></DKTextInput>
-		<DKCheckBox label="Remember me" checked />
-		<div class="flex justify-end">
-			<DKButton btnClass="btn-primary btn-sm">Gửi</DKButton>
-		</div>
-	</DKModal>
+	<div class="flex items-center justify-center pt-[5vh]">
+		<div class="flex w-[280px] h-[140px]"><IConX /><IConO /></div>
+	</div>
+	<div class="flex flex-col items-center mt-[7vh]">
+		<span
+			class="select-none btn-primary mt-2 w-[60%] rounded-[999px] btn inline-flex justify-center"
+			>Tạo Phòng</span
+		>
+		<span
+			class="select-none btn-primary mt-2 w-[60%] rounded-[999px] btn inline-flex justify-center"
+			>Vào Phòng</span
+		>
+		<span
+			class="select-none btn-primary mt-2 w-[60%] rounded-[999px] btn inline-flex justify-center"
+			>Danh Sách Phòng</span
+		>
+	</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IConX from '@/assets/icons/x_icon.svg';
+import IConO from '@/assets/icons/o_icon.svg';
+</script>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
 		Components({
 			resolvers: [ElementPlusResolver()],
 		}),
+		svgLoader(),
 	],
 	resolve: {
 		alias: {
